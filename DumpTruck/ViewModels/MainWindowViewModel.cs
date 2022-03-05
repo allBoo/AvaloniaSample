64176ececb386ab7fc30fc96f063962780ce62e1
Alex K <alboo@list.ru>
2022-03-05 16:08:16 +0400

    Initial Commit

---
+++ DumpTruck/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace DumpTruck.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            BuyMusicCommand = ReactiveCommand.Create(() =>
+            {
+                // Code here will be executed when the button is clicked.
+            });
+        }
+
+        public ICommand BuyMusicCommand { get; }
+    }
+}
---
+++ DumpTruck/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace DumpTruck.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
